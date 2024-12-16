42f8c8c91de611645f14ca0f7f2f467c98e826bd
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-12-17 01:19:10 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-[32px] px-4 sm:px-8 lg:px-16 pt-12 max-sm:gap-[24px] ">
             {/* Logo and Vision */}
             <div className="text-left">
-              <h1 className="font-bold mt-[80px] pl-[40px] gap-[16px] text-[#3563E9] tracking-[-3%] leading-[48px] drop-shadow-md text-[32px]">
+              <h1 className="font-bold mt-[40px] pl-[40px] gap-[16px] text-[#3563E9] tracking-[-3%] leading-[48px] drop-shadow-md text-[32px]">
                 MORENT
               </h1>
               <div className="w-[292px] h-[108px]">
@@ -20,7 +20,7 @@ export default function Footer() {
 
             {/* About Section */}
             <div className=" pl-[350px] max-sm:pl-16 max-sm:pt-28 ">
-              <h2 className="w-[152px] h-[32px] pt-[80px] left-[804px] max-sm:gap-[16px] font-semibold text-[#1A202C]">
+              <h2 className="w-[152px] h-[32px] pt-[40px] left-[804px] max-sm:gap-[16px] font-semibold text-[#1A202C]">
                 About
               </h2>
               <ul className="w-[152px] h-[188px] mt-[40px] text-[#13131399] font-medium text-[16px] leading-[24px] tracking-[-0.02em] space-y-2 ">
@@ -61,7 +61,7 @@ export default function Footer() {
 
             {/* Community Section */}
             <div className=" pl-[220px] max-sm:pl-16">
-              <h2 className="w-[152px] h-[32px] pt-[80px] left-[1016px] font-semibold text-[#1A202C]">
+              <h2 className="w-[152px] h-[32px] pt-[40px] left-[1016px] font-semibold text-[#1A202C]">
                 Community
               </h2>
               <ul className="w-[152px] h-[188px] mt-[40px] text-[#90A3BF] font-medium text-[16px] leading-[24px] tracking-[-0.02em] space-y-2">
@@ -102,7 +102,7 @@ export default function Footer() {
 
             {/* Socials Section */}
             <div className="pl-[110px] max-sm:pl-16">
-              <h2 className="w-[152px] h-[32px] pt-[80px] left-[1228px] font-semibold text-[#1A202C]">
+              <h2 className="w-[152px] h-[32px] pt-[40px] left-[1228px] font-semibold text-[#1A202C]">
                 Socials
               </h2>
               <ul className="w-[152px] h-[188px] mt-[40px] text-[#90A3BF] font-medium text-[16px] leading-[24px] tracking-[-0.02em] space-y-2">
